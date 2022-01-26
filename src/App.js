c5a10e1b968065fe10b1f1635d5ab6034b66b997
Gursharanp <gursharanpreetsinghkang2@gmail.com>
2022-01-26 19:43:32 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Content from './components/Content';
+import Header from './components/Header';
 import './style/App.css';
 
 function App() {
   return (
     <div className="App">
-      HELLO WORLD
+      <Header/>
+    <Content/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+
+        <div className='headbar'>
+            <div className='icon'>
+                <img src="https://blinkit.com/images/header/blinkit_logo-3898547.png" />
+            </div>
+            <div className='address'>
+                    <div className='aicon'></div>
+                    <div className='atext'>
+                        <div className='addhead'> delivery in 15 minutes</div>
+                        <div className='addfoot'> Sector 18D,Sector18, Chandhigarh </div>
+                    </div>
+            </div>
+            <div className='search'>
+                    
+                <input type="search"  placeholder='Search for products'/>
+                <div className='sicon'> <i class="fa fa-search"></i></div>
+                
+            </div>
+            <div className='category'>
+                 categories
+            </div>
+            <div className='login'>
+                   login
+            </div>
+            <div className='my-cart'>
+                <button>
+                <i class="fa fa-shopping-cart"></i>
+                    my Cart
+                </button>
+            </div>
+
+        </div>
+
+
+    )
+
+}
